dad91b7ec9e08dca8843240fad608c3e9e5d6c9d
Dwight Anthony Taylor Jr <dtaylo37@unix3.csc.calpoly.edu>
2026-05-08 17:47:03 -0700

    finalized file structure, made needed edits to files

--- rcopy.c
+++ rcopy.c
@@ -1,14 +1,19 @@
-/* Server side - UDP Code				    */
-/* By Hugh Smith	4/1/2017	*/
+// Client side - UDP Code				    
+// By Hugh Smith	4/1/2017		
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <strings.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
+#include <netdb.h>
 
 #include "gethostbyname.h"
 #include "networks.h"
@@ -16,65 +21,96 @@
 
 #define MAXBUF 80
 
-void processClient(int socketNum);
-int checkArgs(int argc, char *argv[]);
+void talkToServer(int socketNum, struct sockaddr_in6 * server);
+int readFromStdin(char * buffer);
+int checkArgs(int argc, char * argv[]);
 
-int main ( int argc, char *argv[]  )
-{ 
+int main (int argc, char *argv[])
+ {
 	int socketNum = 0;				
+	struct sockaddr_in6 server;		// Supports 4 and 6 but requires IPv6 struct
 	int portNumber = 0;
-
-	portNumber = checkArgs(argc, argv);
-		
-	socketNum = udpServerSetup(portNumber);
-
-	processClient(socketNum);
-
-	close(socketNum);
 	
+	portNumber = checkArgs(argc, argv);
+	
+	socketNum = setupUdpClientToServer(&server, argv[1], portNumber);
+	
+	talkToServer(socketNum, &server);
+	
+	close(socketNum);
+
 	return 0;
 }
 
-void processClient(int socketNum)
+void talkToServer(int socketNum, struct sockaddr_in6 * server)
 {
+	int serverAddrLen = sizeof(struct sockaddr_in6);
+	char * ipString = NULL;
 	int dataLen = 0; 
-	char buffer[MAXBUF + 1];	  
-	struct sockaddr_in6 client;		
-	int clientAddrLen = sizeof(client);	
+	char buffer[MAXBUF+1];
 	
 	buffer[0] = '\0';
 	while (buffer[0] != '.')
 	{
-		dataLen = safeRecvfrom(socketNum, buffer, MAXBUF, 0, (struct sockaddr *) &client, &clientAddrLen);
+		dataLen = readFromStdin(buffer);
+
+		printf("Sending: %s with len: %d\n", buffer,dataLen);
 	
-		printf("Received message from client with ");
-		printIPInfo(&client);
-		printf(" Len: %d \'%s\'\n", dataLen, buffer);
-
-		// just for fun send back to client number of bytes received
-		sprintf(buffer, "bytes: %d", dataLen);
-		safeSendto(socketNum, buffer, strlen(buffer)+1, 0, (struct sockaddr *) & client, clientAddrLen);
-
+		safeSendto(socketNum, buffer, dataLen, 0, (struct sockaddr *) server, serverAddrLen);
+		
+		safeRecvfrom(socketNum, buffer, MAXBUF, 0, (struct sockaddr *) server, &serverAddrLen);
+		
+		// print out bytes received
+		ipString = ipAddressToString(server);
+		printf("Server with ip: %s and port %d said it received %s\n", ipString, ntohs(server->sin6_port), buffer);
+	      
 	}
 }
 
-int checkArgs(int argc, char *argv[])
+int readFromStdin(char * buffer)
 {
-	// Checks args and returns port number
-	int portNumber = 0;
+	char aChar = 0;
+	int inputLen = 0;        
 	
-	if (argc > 3)
+	// Important you don't input more characters than you have space 
+	buffer[0] = '\0';
+	printf("Enter data: ");
+	while (inputLen < (MAXBUF - 1) && aChar != '\n')
 	{
-		fprintf(stderr, "Usage %s [optional port number]\n", argv[0]);
-		exit(-1);
+		aChar = getchar();
+		if (aChar != '\n')
+		{
+			buffer[inputLen] = aChar;
+			inputLen++;
+		}
 	}
 	
-	if (argc == 2)
+	// Null terminate the string
+	buffer[inputLen] = '\0';
+	inputLen++;
+	
+	return inputLen;
+}
+
+int checkArgs(int argc, char * argv[])
+{
+
+    int portNumber = 0;
+	
+    /* check command line arguments  */
+	
+	if (argc != 3)
 	{
-		portNumber = atoi(argv[1]);
+		printf("usage: %s host-name port-number \n", argv[0]);
+		exit(1);
 	}
 	
+	portNumber = atoi(argv[2]);
+		
 	return portNumber;
 }
+
+
+
 
 
